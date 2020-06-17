caf1147710581d0ee4d62a89f0cb4148c33b65a1
oscarhdzlimon <54861099+oscarhdzlimon@users.noreply.github.com>
2020-06-17 17:07:20 -0500

    Se agregaron modulos

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NopagefoundComponent } from './nopagefound/nopagefound.component';
+import { HeaderComponent } from './header/header.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
+
+// import { AppRoutingModule } from './app-routing.module';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  exports:[
+    NopagefoundComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
+  ],
+  providers: []
+})
+export class SharedModule { }
